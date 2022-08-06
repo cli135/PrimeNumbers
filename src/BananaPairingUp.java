3a4414ca6ad451acfd8a02ae94b3e69f2f3da4d8
Christopher Li <cli135@jhu.edu>
2022-08-06 17:32:54 -0400

    4b, tests 1 + 2 passing, remaining 3, 4, 5, so far using O(n!) approach of permutations

--- src/BananaPairingUp.java
+++ src/BananaPairingUp.java
@@ -1,24 +1,79 @@
-import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+
 public class BananaPairingUp {
+
+    // global variables carry across function calls
+    // including across different unit tests
+    // so you just want to parameter pass instead
+    // so it goes out of scope and is recreated
+    // when necessary
     public static int solution(int[] banana_list) {
         // can't possibly check all possible ways using brute force
         // would be something on the order of O(n!)
         // due to permutations of matching pairs of people up
         // look for an optimization: greedy, sorted array, two pointers
         // etc.
-
-        Arrays.sort(banana_list);
+        Integer[] bananas_arr = new Integer[banana_list.length];
+        for (int i = 0; i < banana_list.length; i++) {
+            bananas_arr[i] = banana_list[i];
+        }
+        ArrayList<Integer> bananas = new ArrayList<>();
+        List<Integer> origBananas = new ArrayList<>();
+        for (Integer i : banana_list) {
+            origBananas.add(i);
+            bananas.add(i);
+        }
+        //Arrays.sort(banana_list);
 
         // how can you tell, ahead of time, (by looking at them in O(1))
         // that two numbers will cycle? << the big question
 
+        // maybe you can't
+        // maybe it's algorithmic in nature
+        // well i don't know for sure
+        // but maybe part of the solution is to cache it
+        // well for the other part,
+        // even if you can do cycle(x, y) in O(1)
+        // how are you going to get around checking all O(n!) possibilites?
 
+        // alright let's just write the O(n!) algorithm
+        return pairOffNums(bananas, 0, origBananas.size());
 
+        //return 0; // method stub
+    }
 
-        return 0; // method stub
+    public static int pairOffNums(ArrayList<Integer> bananas, int start, int origSize) {
+        if (start >= bananas.size() - 1) {
+            return origSize;
+        }
+        ArrayList<Integer> results = new ArrayList<>();
+//        results.add(pairOffNums(bananas, start + 1));
+        for (int i = start + 1; i < bananas.size(); i++) {
+            // please avoid mixing up indices with elements
+            if (cycle(bananas.get(start), bananas.get(i))) {
+                Integer saved1 = bananas.remove(i);
+                Integer saved2 = bananas.remove(start);
+                results.add(-2 + pairOffNums(bananas, start, origSize));
+                bananas.add(start, saved2);
+                bananas.add(i, saved1);
+
+            }
+            results.add(0 + pairOffNums(bananas, start + 1, origSize));
+        }
+
+        // minimum of results
+        Integer min = Integer.MAX_VALUE;
+        for (Integer i : results) {
+            if (i < min) {
+                min = i;
+            }
+        }
+        return min;
     }
 
     public static boolean cycle(int x, int y) {
--- src/TestBanana.java
+++ src/TestBanana.java
@@ -1,5 +1,7 @@
 public class TestBanana {
     public static void main(String[] args) {
+        System.out.println(BananaPairingUp.solution(new int[] {1, 1}));
+        System.out.println(BananaPairingUp.solution(new int[] {1, 7, 3, 21, 13, 19}));
 
     }
 }
