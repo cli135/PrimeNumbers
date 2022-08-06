9ac4ebc541a9d694ea41b3610be3eaf132c48607
Christopher Li <cli135@jhu.edu>
2022-08-06 18:02:41 -0400

    4b, testing odd and even number differences for cycle inputs

--- src/TestBanana.java
+++ src/TestBanana.java
@@ -2,6 +2,15 @@ public class TestBanana {
     public static void main(String[] args) {
         System.out.println(BananaPairingUp.solution(new int[] {1, 1}));
         System.out.println(BananaPairingUp.solution(new int[] {1, 7, 3, 21, 13, 19}));
+        System.out.println(BananaPairingUp.solution(new int[] {1, 2, 3, 4, 5, 6, 7, 50, 53}));
+        for (int i = 1; i < 10000; i++) {
+            int j = i + 7;
+            if (BananaPairingUp.cycle(i, j) == false) {
+                System.out.println("exception in " + i + " and " + j);
+            }
+        }
+        System.out.println("none so far");
+
 
     }
 }
